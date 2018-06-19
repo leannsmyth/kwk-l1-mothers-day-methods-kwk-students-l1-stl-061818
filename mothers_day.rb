73181fabb3c2ee754cd0950ffcfa129b3ebd6267
leannsmyth <leannmsmyth@gmail.com>
2018-06-19 15:47:11 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,9 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def mothers_day (greeting= "Happy Mother's Day, Mom!")
+  puts greeting
+end
+
+mothers_day
+
 
