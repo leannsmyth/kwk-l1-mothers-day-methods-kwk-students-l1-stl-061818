3c5ab6992d1b05e4175bf854f8af3afc36a228cb
leannsmyth <leannmsmyth@gmail.com>
2018-06-19 15:48:47 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,6 +1,6 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-def mothers_day (greeting= "Happy Mother's Day, Mom!")
+def mothers_day (greeting= "Happy Mother's Day, Grandma!")
   puts greeting
 end
 
